84f77bb11d12726e12e5256d0af3cb153b53170f
Rohit-Rajvanshi <ryugarohit123@gmail.com>
2023-11-11 05:34:56 +0530

    Home-page 80%done

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const themeBtn=document.querySelector(".theme")
 const header=document.querySelector(".header")
 const btn=document.querySelectorAll(".btn")
+const body=document.body;
+const mSec=document.querySelectorAll(".m-sec")
+const mInfo=document.querySelector(".m-info")
 
 themeBtn.addEventListener("click",()=>{
     header.classList.toggle("light")
+    mInfo.classList.toggle("m-info-change")
+    body.classList.toggle("body")
+    mSec.forEach((sec)=>{
+        sec.classList.toggle("mSec-change")
+    });
     btn.forEach((button) => { 
         button.classList.toggle("btn-change");
     });
